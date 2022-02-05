84933348e7a99c1093d87d451e1b50a58c8c2a5f
berk_73 <sdtbrk93@gmail.com>
2022-02-04 22:02:03 -0600

    nerw

--- src/test/java/com/cybertek/pages/gitexamples.java
+++
@@ -1,9 +0,0 @@
-package com.cybertek.pages;
-
-public class gitexamples {
-    public static void main(String[] args) {
-        System.out.println("this is new item to edd may java i just want to see how its working");
-
-
-    }
-}
