0d1518f46dd21cc0fdbab065de826b809a9b67eb
sedat_73 <sdtbrk93@gmail.com>
2022-02-19 20:30:05 -0600

    new story

--- src/test/java/com/cybertek/stepdefs/HelloWorldApiStepDefs.java
+++ src/test/java/com/cybertek/stepdefs/HelloWorldApiStepDefs.java
@@ -5,18 +5,23 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import static io.restassured.RestAssured.given;
 import static org.junit.Assert.assertEquals;
 
 public class HelloWorldApiStepDefs {
-
+    private static final Logger LOG= LogManager.getLogger();
     Response response;
 
     @Given("User sends get request to hello world api")
     public void user_sends_get_request_to_hello_world_api() {
+        LOG.info("Sending GET Request to "+ConfigurationReader.getProperty("hello.world.api"));
+
         response = given().accept(ContentType.JSON)
                 .when().get(ConfigurationReader.getProperty("hello.world.api"));
+        LOG.info("Completed GET Request to "+ConfigurationReader.getProperty("hello.world.api")+"With response "+response.asString());
     }
 
     @Then("status code is {int}")
